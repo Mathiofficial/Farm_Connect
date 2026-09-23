48fd25bdb6b15dc50df9fc397634882646bb3615
Mathiofficial <mathiyazhagan3112@gmail.com>
2026-09-23 10:39:09 +0530

    Farm Connect

--- src/Test/Basic.test.js
+++ src/Test/Basic.test.js
@@ -2,6 +2,6 @@ import { describe, it, expect } from 'vitest'
 
 describe('Basic test', () => {
   it('checks that the application is working', () => {
-    expect(1 + 1).toBe(5)
+    expect(1 + 1).toBe(2)
   })
 })
